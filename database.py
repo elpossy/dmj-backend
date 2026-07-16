c4a550b466ad83e1b73f7b17f3f29f63c25e84d0
elpossy <bossyfaycal5@gmail.com>
2026-07-16 23:47:57 +0100

    Add procfile.txt/correction de bug(commit->upload)
    Correction de bug, ajout du fichier procfile.txt pour forcer le déploiement. Import de fichier via upload

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise RuntimeError("❌ DATABASE_URL manquant dans .env")
+    raise RuntimeError("❌ DATABASE_URL manquant dans les variables environnements")
 
 engine = create_engine(
     DATABASE_URL,
